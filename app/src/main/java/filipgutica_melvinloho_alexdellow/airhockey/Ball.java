b277a21cf339c7f02718e40efbb5f6c5bbcd491d
filip <fgutica@gmail.com>
2014-11-02 15:57:12 -0800

    blajblajblajbadsfhasdkjhalfjahflkajhflasjfdhlfjahdlakjfhalsdkjhasdlkjf

---
+++ app/src/main/java/filipgutica_melvinloho_alexdellow/airhockey/Ball.java
@@ -0,0 +1,100 @@
+package filipgutica_melvinloho_alexdellow.airhockey;
+
+import android.graphics.Bitmap;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Filip on 2014-09-16.
+ * Based on code from...
+ *  Modified by Melvin and Filip
+ */
+public class Ball {
+
+    float frictionFactor = 1.00001f;
+
+    public static ArrayList<Ball> balls = new ArrayList<Ball>();
+
+
+    float x;
+    float y;
+    float speed_x;
+    float speed_y;
+    public int ballRadius;
+    Bitmap curBall;
+
+    public Ball( Bitmap img, float xPos, float yPos, int rad)
+    {
+        speed_x = 0;
+        speed_y = 0;
+        x = xPos;
+        y = yPos;
+        ballRadius = rad;
+        curBall = img;
+
+        balls.add(this);
+
+    }
+
+    public Bitmap getBitmap() {
+
+        return curBall;
+    }
+
+    public void update() {
+        x += speed_x;
+        y += speed_y;
+        if (x >= 300 - ballRadius && speed_x > 0) speed_x = Math.abs(speed_x);
+        if (x <= ballRadius && speed_x < 0) speed_x = -Math.abs(speed_x);
+        if (y >= 200 - ballRadius && speed_y > 0) speed_y = Math.abs(speed_y);
+        if (y <= ballRadius && speed_y < 0) speed_y = -Math.abs(speed_y);
+
+        speed_x *= frictionFactor;
+        speed_y *= frictionFactor;
+    }
+
+    public void detectCollisions() {
+        for (Ball b : balls) {
+            if (b != this) {
+                if (x + ballRadius + b.ballRadius > b.x
+                        && x < b.x + ballRadius + b.ballRadius
+                        && y + ballRadius + b.ballRadius > b.y
+                        && y < b.y + ballRadius + b.ballRadius) {
+                    if (distanceTo(this, b) < ballRadius + b.ballRadius) {
+                        calculateNewVelocities(this, b);
+                    }
+                }
+            }
+        }
+    }
+
+    public double distanceTo(Ball a, Ball b) {
+        return Math.abs(Math.sqrt(((a.x - b.x) * (a.x - b.x)) + ((a.y - b.y) * (a.y - b.y))));
+    }
+
+    public void calculateNewVelocities(Ball firstBall, Ball secondBall) {
+        float mass1 = firstBall.ballRadius;
+        float mass2 = secondBall.ballRadius;
+        float velX1 = firstBall.speed_x;
+        float velX2 = secondBall.speed_x;
+        float velY1 = firstBall.speed_y;
+        float velY2 = secondBall.speed_y;
+
+        float newVelX1 = (velX1 * (mass1 - mass2) + (2 * mass2 * velX2)) / (mass1 + mass2);
+        float newVelX2 = (velX2 * (mass2 - mass1) + (2 * mass1 * velX1)) / (mass1 + mass2);
+        float newVelY1 = (velY1 * (mass1 - mass2) + (2 * mass2 * velY2)) / (mass1 + mass2);
+        float newVelY2 = (velY2 * (mass2 - mass1) + (2 * mass1 * velY1)) / (mass1 + mass2);
+
+        firstBall.speed_x = newVelX1;
+        secondBall.speed_x = newVelX2;
+        firstBall.speed_y = newVelY1;
+        secondBall.speed_y = newVelY2;
+
+        firstBall.x = firstBall.x + newVelX1;
+        firstBall.y = firstBall.y + newVelY1;
+        secondBall.x = secondBall.x + newVelX2;
+        secondBall.y = secondBall.y + newVelY2;
+
+
+    }
+}
---
+++ app/src/main/java/filipgutica_melvinloho_alexdellow/airhockey/HockeyArena.java
@@ -0,0 +1,238 @@
+package filipgutica_melvinloho_alexdellow.airhockey;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.View;
+
+/**
+ * Created by Filip on 2014-09-15.
+ */
+public class HockeyArena extends View  {
+
+
+    final int PADDLE = 1;
+    final int PUCK = 2;
+
+    private Paint mPaint = new Paint();         // Paint to draw set color etc...
+    private Bitmap paddle;                      // First Paddle img
+    private Bitmap puck;                        // Puck img
+    private Bitmap paddle2;                     // Second paddle img
+    private Ball paddleBall2;                   // Second paddle. Ball object to give it Collision detection and trakc speed
+    private Ball paddleBall;                    // First paddle. "" ""
+    private Ball puckBall;                      // Puck "" ""
+    private float paddleWidth;                  // Width of a paddle
+    private float paddleHeight;                 // Height of a paddle
+    private float puckWidth;                    // Width of a puck
+    private float puckHeight;                   // Height of a puck
+    private VelocityTracker velocity = null;    // Used to track velocity of your finger as it swipes along the screen
+    private VelocityTracker velocity2 = null;    // "" ""
+
+    public HockeyArena(Context context) {
+        super(context);
+        commonConstructor();
+    }
+    public HockeyArena(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        commonConstructor();
+    }
+
+    public void commonConstructor() {
+
+      // mPaint.setColor(Color.BLUE);
+        paddle = BitmapFactory.decodeResource(getResources(),R.drawable.blue_paddle);
+        paddle2 = BitmapFactory.decodeResource(getResources(), R.drawable.green_paddle);
+        puck = BitmapFactory.decodeResource(getResources(), R.drawable.puck);
+
+        paddleWidth = paddle.getWidth();
+        paddleHeight = paddle.getHeight();
+        puckWidth = puck.getWidth();
+        puckHeight = puck.getHeight();
+
+        paddleBall2 = new Ball(paddle2, 200f, 500f, (int)(paddleHeight)/2);
+        paddleBall = new Ball(paddle, 400f, 700f, (int) (paddleHeight/2));
+        puckBall = new Ball(puck, 500f, 800f, (int)(puckWidth/2) );
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent m) {
+
+        int action = m.getActionMasked();
+        int index = m.getActionIndex();
+        int id;
+
+        if (index > 0) {
+            Log.d("", "MULTITOUCH");
+            paddleBall2.x = m.getX(index);
+            paddleBall2.y = m.getY(index);
+        }
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                // Get the number of pointers
+                int pointerCount = m.getPointerCount();
+
+                for (int i = 0; i < pointerCount; i ++) {
+                    // Get the pointer ids
+                    id = m.getPointerId(i);
+                    // First Pointer
+                    if (id == 0) {
+                        paddleBall.x = m.getX(i);
+                        paddleBall.y = m.getY(i);
+                        paddleBall.speed_x = 0;
+                        paddleBall.speed_y = 0;
+
+                        if (velocity == null) {
+                            // Retrieve a new VelocityTracker object to watch the velocity of a motion.
+                            velocity = VelocityTracker.obtain();
+                            velocity2 = VelocityTracker.obtain();
+                        } else {
+                            // Reset the velocity tracker back to its initial state.
+                            velocity.clear();
+                            velocity2.clear();
+                        }
+                        // Add a user's movement to the tracker.
+                        velocity.addMovement(m);
+                        velocity2.addMovement(m);
+                    }
+                    // Second pointer
+                    else if (id == 1) {
+                        paddleBall2.x = m.getX(i);
+                        paddleBall2.y = m.getY(i);
+                        paddleBall2.speed_x = 0;
+                        paddleBall2.speed_y = 0;
+
+                        if (velocity2 == null) {
+                            velocity = VelocityTracker.obtain();
+                            velocity2 = VelocityTracker.obtain();
+                        } else {
+                            velocity.clear();
+                            velocity2.clear();
+                        }
+                        velocity.addMovement(m);
+                        velocity2.addMovement(m);
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                //Number of pointers
+                int pointerCount2 = m.getPointerCount();
+
+                for (int i = 0; i < pointerCount2; i++){
+                    id = m.getPointerId(i);
+                    if (id == 0) {
+                        paddleBall.update();
+                        paddleBall.detectCollisions();
+                        paddleBall.x = m.getX(i);
+                        paddleBall.y = m.getY(i);
+                        //Add movement to tracker
+                        velocity.addMovement(m);
+                        // When you want to determine the velocity, call
+                        // computeCurrentVelocity(). Then call getXVelocity()
+                        // and getYVelocity() to retrieve the velocity in pixels/10ms
+                        velocity.computeCurrentVelocity(10);
+                        paddleBall.speed_x = velocity.getXVelocity(i);
+                        paddleBall.speed_y = velocity.getYVelocity(i);
+                    }
+                    if (id == 1) {
+                        paddleBall2.update();
+                        paddleBall2.detectCollisions();
+                        paddleBall2.x = m.getX(i);
+                        paddleBall2.y = m.getY(i);
+                        velocity2.addMovement(m);
+                        velocity2.computeCurrentVelocity(10);
+                        paddleBall2.speed_x = velocity2.getXVelocity(i);
+                        paddleBall2.speed_y = velocity2.getYVelocity(i);
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                paddleBall.speed_x = 0;
+                paddleBall.speed_y = 0;
+                paddleBall2.speed_x = 0;
+                paddleBall2.speed_y = 0;
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                paddleBall.speed_x = 0;
+                paddleBall.speed_y = 0;
+                paddleBall2.speed_x = 0;
+                paddleBall2.speed_y = 0;
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                // Return a VelocityTracker object back to be re-used by others.
+                //velocity.recycle();
+                break;
+        }
+
+        return true;
+    }
+
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        mPaint.setColor(Color.BLUE);
+        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        canvas.drawARGB(255, 255, 255, 255);
+
+        mPaint.setAntiAlias(true);
+
+        canvas.drawBitmap(puckBall.getBitmap(), puckBall.x - puckWidth/2, puckBall.y - puckWidth/2, mPaint);
+        canvas.drawBitmap(paddleBall2.getBitmap(), paddleBall2.x - paddleWidth / 2, paddleBall2.y - paddleWidth / 2, mPaint);
+        canvas.drawBitmap(paddleBall.getBitmap(), paddleBall.x - paddleWidth / 2, paddleBall.y - paddleWidth / 2, mPaint);
+
+
+        for (Ball b : Ball.balls) b.update();
+        for (Ball b : Ball.balls) b.detectCollisions();
+        for (Ball b : Ball.balls) detectWallCollisions(b, canvas);
+        /*Only if balls touch walls*/
+        //detectWallCollisions(paddleBall, canvas);
+        //detectWallCollisions(paddleBall2, canvas);
+        //detectWallCollisions(puckBall, canvas);
+
+        invalidate();
+    }
+
+    public void detectWallCollisions(Ball b, Canvas c) {
+
+        Ball paddleBall = b;
+        Canvas canvas = c;
+
+        //when paddle hits left wall
+        if (paddleBall.x < 0 + paddleWidth/2) {
+            paddleBall.speed_x = Math.abs(paddleBall.speed_x);
+            paddleBall.x = 0 + paddleHeight/2 ;
+
+            //when paddle hits right wall
+        } else if ( paddleBall.x >= canvas.getWidth()- paddleWidth/2) {
+
+            paddleBall.speed_x= -Math.abs(paddleBall.speed_x);
+            paddleBall.x = canvas.getWidth() - paddleWidth/2;
+        }
+        //paddle hits top wall
+        if (paddleBall.y < /*&canvas.getHeight()/2*/ 0 + paddleHeight/2) {
+
+            paddleBall.speed_y = Math.abs(paddleBall.speed_y);
+            paddleBall.y = 0 + paddleHeight/2;
+
+            //paddle hits bottom wall
+        } else if (paddleBall.y > canvas.getHeight() - paddleHeight/2) {
+
+            paddleBall.speed_y = -Math.abs(paddleBall.speed_y);
+            paddleBall.y = canvas.getHeight() - paddleHeight/2;
+        }
+
+    }
+
+}
+
+
+
+
